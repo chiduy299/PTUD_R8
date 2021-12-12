4522c66d65ed5d911232e6f1d5aa0d32e5e7e5a9
chiduy299 <51262175+chiduy299@users.noreply.github.com>
2021-12-12 14:35:35 +0700

    structure project ptud r8

---
+++ ptud_project/Data/Customer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ptud_project.Data
+{
+    public class Customer
+    {
+        [Key]
+        public Guid id_cus { get; set; }
+        [Required]
+        [MinLength(1),MaxLength(100)]
+        public string name { get; set; }
+        [Required]
+        public Int64 created_at { get; set; }
+        public Int64 update_at { get; set; }
+        public string cmnd { get; set; }
+        public string address { get; set; }
+        [Required]
+        [MaxLength(11)]
+        public string phone { get; set; }
+        [Required]
+        [MinLength(8)]
+        public string password { get; set; }
+        [Required]
+        [Range(0, 2)]
+        public Int16 sex { get; set; }
+
+    }
+}
---
+++ ptud_project/Migrations/20211209132420_DbInit.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ptud_project.Migrations
+{
+    public partial class DbInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    user_name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at = table.Column<long>(type: "bigint", nullable: false),
+                    avartar_url = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.user_name);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
