2eff6a3218971c34d99eda5953b7ec5b87b43ea9
chiduy299 <51262175+chiduy299@users.noreply.github.com>
2022-01-14 00:43:27 +0700

    add login admin

---
+++ ptud_project/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using ptud_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ptud_project.Controllers
+{
+    public class AdminController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AdminController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public IActionResult LoginAdmin([FromBody] LoginCustomerModel request)
+        {
+            try
+            {
+                var key = _configuration.GetValue<string>("admin");
+                return Ok(new { code = 0, message = "Bad Request", payload = key });
+            }
+            catch
+            {
+                return Ok(new { code = -401, message = "Bad Request" });
+            }
+        }
+
+    }
+}
